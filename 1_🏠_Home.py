b73512e29b87d96237948589f63dcd64741a48cd
Felippe Andrade <4drade@gmail.com>
2024-02-20 09:53:50 -0300

    refactor: data loading function and add Kaggle link button
    Refactor data loading function to use keyword-only arguments for clarity and consistency. Add a link button to Kaggle for easier access to the dataset. Update dataframe assignment to session state for improved data handling.

--- 1_🏠_Home.py
+++ 1_🏠_Home.py
@@ -1,5 +1,4 @@
 import re
-import webbrowser
 from datetime import datetime
 
 import pandas as pd
@@ -9,27 +8,22 @@ st.set_page_config(page_title="Home", page_icon="🏠", layout="centered")
 
 
 @st.cache_data
-def load_data(file_csv: str, nrows: None = None):
-    data = pd.read_csv(file_csv, nrows=nrows, index_col=0)
+def load_data(*, file_csv: str, num_rows: None = None) -> pd.DataFrame:
+    data = pd.read_csv(file_csv, nrows=num_rows, index_col=0)
     data = data[data["Contract Valid Until"] >= datetime.now().year]
     data = data[data["Value(£)"] > 0]
-    data = data.sort_values(by="Overall", ascending=False)
+    data.sort_values(by="Overall", ascending=False, inplace=True)
     data.rename(lambda x: str(x).lower(), axis="columns", inplace=True)
     # remove numbers in name column with regex and strip whitespace
     data["name"] = data["name"].apply(lambda x: re.sub(r"\d+", "", x).strip())
     return data
 
 
+st.link_button(
+    label="Access data on Kaggle",
+    url="https://www.kaggle.com/datasets/kevwesophia/fifa23-official-datasetclean-data",
+)
 st.title(f"FIFA23 Official Dataset")
-
-btn = st.button("Access data on Kaggle")
-if btn:
-    webbrowser.open_new_tab(
-        "https://www.kaggle.com/datasets/kevwesophia/fifa23-official-datasetclean-data"
-    )
-
-st.session_state["df_data"] = load_data("datasets/CLEAN_FIFA23_official_data.csv")
-
 st.markdown(
     """
     ## About this dataset
@@ -43,5 +37,8 @@ st.markdown(
     player positioning, and player development over time.
     """
 )
-
 st.sidebar.caption("Made with ❤️ by [Felippe A.]")
+
+st.session_state["df_data"] = load_data(
+    file_csv="datasets/CLEAN_FIFA23_official_data.csv"
+)
