0937f51a2ce84d8af0a3bf6b95cbeddc41d37e5f
Felippe Andrade <4drade@gmail.com>
2024-02-22 08:59:18 -0300

    refactor: Home page layout and add menu items
    Refactored the Home page layout to center content and added menu items for reporting bugs and about information. Incorporated buttons to access and download the dataset. Updated URLs for dataset access and code reference. The changes aim to enhance user experience and provide more seamless access to information.

--- 1_🏠_Home.py
+++ 1_🏠_Home.py
@@ -4,17 +4,24 @@ from datetime import datetime
 import pandas as pd
 import streamlit as st
 
-st.set_page_config(page_title="Home", page_icon="🏠", layout="centered")
+st.set_page_config(
+    page_title="Home",
+    page_icon="🏠",
+    layout="centered",
+    menu_items={
+        "Report a bug": "https://github.com/bysedd/FIFA-23/issues/new",
+        "About": "FIFA23 OFFICIAL DATASET CLEAN FROM 2023",
+    },
+)
 
 
 @st.cache_data
 def load_data(*, file_csv: str, num_rows: None = None) -> pd.DataFrame:
     data = pd.read_csv(file_csv, nrows=num_rows, index_col=0)
-    data = data[data["Contract Valid Until"] >= datetime.now().year]
     data = data[data["Value(£)"] > 0]
     data.sort_values(by="Overall", ascending=False, inplace=True)
     data.rename(lambda x: str(x).lower(), axis="columns", inplace=True)
-    
+
     data["name"] = data["name"].apply(lambda x: re.sub(r"\d+", "", x).strip())
     data["Weight(Kg.)"] = data["weight(lbs.)"].apply(lambda x: x * 0.453592)
     data["Height(m.)"] = data["height(cm.)"].apply(lambda x: x / 100)
@@ -22,11 +29,23 @@ def load_data(*, file_csv: str, num_rows: None = None) -> pd.DataFrame:
     return data
 
 
-st.title("FIFA23 Official Dataset")
-st.link_button(
-    label="Access data on Kaggle",
-    url="https://www.kaggle.com/datasets/kevwesophia/fifa23-official-datasetclean-data",
+st.session_state["df_data"] = load_data(
+    file_csv="resources/CLEAN_FIFA23_official_data.csv"
 )
+
+st.title("FIFA23 Official Dataset")
+col1, col2, *_ = st.columns(4)
+with col1:
+    st.link_button(
+        label="Access data",
+        url="https://www.kaggle.com/datasets/kevwesophia/fifa23-official-datasetclean-data",
+    )
+with col2:
+    st.download_button(
+        label="Download data",
+        data=st.session_state["df_data"].reset_index().to_csv(),
+        file_name="CLEAN_FIFA23_official_data.csv",
+    )
 st.markdown(
     """
     ## About this dataset
@@ -40,7 +59,4 @@ st.markdown(
     """
 )
 st.sidebar.caption("Made with ❤️ by [Felippe A.](https://linkedin.com/in/bysedd/)")
-
-st.session_state["df_data"] = load_data(
-    file_csv="datasets/CLEAN_FIFA23_official_data.csv"
-)
+st.sidebar.caption("Check the code on [GitHub](https://github.com/bysedd/FIFA-23/)")
