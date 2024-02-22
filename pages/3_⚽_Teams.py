d338d8b15b6759504d8446b53b15e16b5ddd3387
Felippe Andrade <4drade@gmail.com>
2024-02-22 08:56:37 -0300

    refactor: club stats display and formatting
    Consolidated club stats for improved readability and added currency formatting for market value, wage, and release clause columns. Also adjusted units for player height and provided column help tooltips for better user guidance. These changes enhance the presentation and usability of the team stats page.

--- pages/3_⚽_Teams.py
+++ pages/3_⚽_Teams.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+from resources.helper_functions import format_currency
 
 st.set_page_config(page_title="Teams", page_icon="⚽", layout="wide")
 
@@ -21,48 +22,54 @@ def display_club_stats(club_df):
         lambda x: str(x).title(), axis="columns", inplace=True
     )  # rename columns to title case
 
-    # format the value columns
-    for value_column in ["Value(£)", "Release Clause(£)"]:
-        club_df[value_column] = club_df[value_column].apply(
-            lambda x: f"£ {int(x):,.0f}"
-        )
+    club_stats = pd.DataFrame(
+        {
+            "Name": club_df["Name"],
+            "Age": club_df["Age"],
+            "Photo": club_df["Photo"],
+            "Position": club_df["Position"],
+            "Flag": club_df["Flag"],
+            "Overall": club_df["Overall"],
+            "Joined": club_df["Joined"],
+            "Height(M.)": club_df["Height(M.)"],
+            "Weight(Kg.)": club_df["Weight(Kg.)"],
+            "Contract Valid Until": club_df["Contract Valid Until"],
+            "Wage(£)": club_df["Wage(£)"],
+            "Value(£)": club_df["Value(£)"],
+            "Release Clause(£)": club_df["Release Clause(£)"],
+        }
+    )
+
+    # # Apply the formatting function to the desired columns
+    # club_stats[["Release Clause(£)", "Value(£)", "Wage(£)"]] = club_stats[
+    #     ["Release Clause(£)", "Value(£)", "Wage(£)"]
+    # ].apply(format_currency)
 
     st.dataframe(
-        club_df[
-            [
-                "Name",
-                "Age",
-                "Photo",
-                "Flag",
-                "Wage(£)",
-                "Overall",
-                "Joined",
-                "Height(Cm.)",
-                "Weight(Lbs.)",
-                "Contract Valid Until",
-                "Value(£)",
-                "Release Clause(£)",
-            ]
-        ],
+        club_stats,
         hide_index=True,
         column_config={
-            "Age": st.column_config.NumberColumn(
-                label="Age", format="%d", help="Age in years"
-            ),
-            "Photo": st.column_config.ImageColumn(label="Photo", help="Player's photo"),
+            "Age": st.column_config.NumberColumn(format="%d", help="Age in years"),
+            "Photo": st.column_config.ImageColumn(help="Player's photo"),
+            "Position": st.column_config.TextColumn(help="Player's position"),
             "Flag": st.column_config.ImageColumn(label="Country"),
-            "Value(£)": st.column_config.Column(
-                label="Market Value", help="Market value of the player"
-            ),
-            "Wage(£)": st.column_config.ProgressColumn(
-                label="Weekly Wage",
+            "Value(£)": st.column_config.NumberColumn(
                 format="£ %d",
-                min_value=0,
-                max_value=club_df["Wage(£)"].max(),
-                width="medium",
+                label="Market Value",
+                help="Market value of the player",
+            ),
+            "Wage(£)": st.column_config.NumberColumn(
+                format="£ %d",
+                label="Weekly Wage",
+                help="Weekly wage of the player",
+            ),
+            "Release Clause(£)": st.column_config.NumberColumn(
+                format="£ %d",
+                label="Release Clause",
+                help="Release clause value",
             ),
             "Overall": st.column_config.ProgressColumn(
-                label="Overall", format="%d", min_value=0, max_value=100, width="medium"
+                format="%d", min_value=0, max_value=100, width="medium"
             ),
             "Joined": st.column_config.DateColumn(
                 label="Joined Club",
@@ -70,16 +77,14 @@ def display_club_stats(club_df):
                 help="Date that the player joined the club",
             ),
             "Contract Valid Until": st.column_config.NumberColumn(
-                label="Contract Valid Until",
                 format="%d",
                 help="Contract valid until year end",
             ),
-            "Release Clause(£)": st.column_config.Column("Release Clause"),
-            "Height(Cm.)": st.column_config.NumberColumn(
-                "Height", format="%d", help="Height in centimeters"
+            "Height(M.)": st.column_config.NumberColumn(
+                label="Height", format="%.2f", help="Height in meters", width="small"
             ),
-            "Weight(Lbs.)": st.column_config.NumberColumn(
-                "Weight", format="%d", help="Weight in pounds"
+            "Weight(Kg.)": st.column_config.NumberColumn(
+                label="Weight", format="%d", help="Weight in kilograms", width="small"
             ),
         },
         use_container_width=True,
@@ -91,13 +96,11 @@ def display_more_info(club_df: pd.DataFrame) -> None:
     team_info = pd.DataFrame(
         {
             "Total players": [club_df.shape[0]],
-            "Total market value": [
-                f"£ {club_df['Value(£)'].apply(lambda x: int(x[2:].replace(',', ''))).sum():,.0f}"
-            ],
+            "Total market value": [f"£ {club_df['Value(£)'].sum():,.0f}"],
             "Weekly player spend": [f"£ {club_df['Wage(£)'].sum():,.0f}"],
             "Average age": [round(club_df["Age"].mean())],
             "Overall average": [club_df["Overall"].mean()],
-            "Average height": [f"{club_df['Height(Cm.)'].mean():.2f} cm"],
+            "Average height": [f"{club_df['Height(M.)'].mean():.2f} m"],
             "Average weight": [f"{club_df['Weight(Kg.)'].mean():.2f} kg"],
             "Most contract end": [club_df["Contract Valid Until"].mode().iloc[0]],
         }
@@ -149,14 +152,16 @@ def display_more_info(club_df: pd.DataFrame) -> None:
     st.divider()
 
     # Cria uma tabela com as médias por posição
-    position_averages = club_df.groupby("Position")[
-        ["Age", "Overall", "Height(Cm.)", "Weight(Kg.)"]
-    ].mean().round(decimals=0)
+    position_averages = (
+        club_df.groupby("Position")[["Age", "Overall", "Height(M.)", "Weight(Kg.)"]]
+        .mean()
+        .round(decimals=0)
+    )
 
     fig = px.bar(
         position_averages,
         x=position_averages.index,
-        y=["Age", "Overall", "Height(Cm.)", "Weight(Kg.)"],
+        y=["Age", "Overall", "Height(M.)", "Weight(Kg.)"],
         title="Averages by position",
         labels={"variable": "Metrics", "value": "Average"},
         color_discrete_sequence=px.colors.qualitative.Pastel,
@@ -212,7 +217,4 @@ def main():
 
 
 if __name__ == "__main__":
-    try:
-        main()
-    except KeyError:
-        st.error("Please go to the **🏠 Home** first. Then back to this page.")
+    main()
